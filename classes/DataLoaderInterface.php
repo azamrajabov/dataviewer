0e73ec0b529956bc674a9e9f3aab30a30d4bc1cf
azamrajabov <arajabov@dealersocket.com>
2018-04-10 11:22:55 -0500

    push to branch

---
+++ classes/DataLoaderInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: azam
+ * Date: 09/04/2018
+ * Time: 20:10
+ */
+
+namespace DataViewer;
+
+/**
+ * Interface DataLoaderInterface
+ * @package DataViewer
+ */
+interface DataLoaderInterface
+{
+    public function getDataInArray();
+}
